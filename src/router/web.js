51302074b9aa8a3124978e0b53c7ec2ad5248cbd
xuantan123 <phanxuantan123@gmail.com>
2025-11-10 10:02:50 +0700

    update code

--- src/router/web.js
+++ src/router/web.js
@@ -1,6 +1,5 @@
 import express from "express";
-import { executeAuthorizedTransfer , checkBalanceMeme , checkBalanceUser } from "../controllers/test2_signMessage.js";
-import { createSignaturePayload } from "../controllers/signatureController.js";
+import { executeAuthorizedTransfer , checkBalanceMeme , checkBalanceUser } from "../controllers/Mintx402.js";
 
 const router = express.Router();
 
@@ -10,7 +9,5 @@ router.get("/hello", (req, res) => {
   router.post("/executeAuthorizedTransfer",executeAuthorizedTransfer);
   router.get("/checkBalanceMeme",checkBalanceMeme);
   router.post("/checkBalanceUser", checkBalanceUser);
-  router.post("/payload", createSignaturePayload);
-
 
 export default router;
